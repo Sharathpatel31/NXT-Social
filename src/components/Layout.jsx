4897828dca8f2fd403b67b7061c4579c0f0e02be
Sharathpatel31 <chintapoolasharath@gmail.com>
2024-08-10 14:53:01 +0530

    final

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Container fluid>
       <Row>
-        <Col md={3} lg={2} className="d-none d-md-block bg-light sidebar">
-          <Sidebar />
+        <Col md={3} lg={2} className="p-0">
+          <Sidebar show={show} handleClose={handleClose} />
         </Col>
         <Col md={9} lg={10} className="ms-sm-auto px-md-4">
+          <Button variant="" className="d-md-none mt-2 mb-2 shadow" onClick={handleShow}>
+           <img  src='./ham.svg' alt="Menu Icon" style={{ width: '24px', height: '24px' }}></img>
+          </Button>
           <main className="py-3">
             {children}
           </main>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,60 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Nav, Offcanvas } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
-const Sidebar = () => {
-  return (
-    <Nav className="flex-column bg-light sidebar">
+const Sidebar = ({ show, handleClose }) => {
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+
+  const NavContent = () => (
+    <Nav className="flex-column">
       <Nav.Item>
-        <Nav.Link as={Link} to="/" className="text-dark">
+        <Nav.Link as={Link} to="/" className="text-dark" onClick={handleClose}>
           <i className="bi bi-house me-2"></i>
           Home
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} to="/users" className="text-dark">
+        <Nav.Link as={Link} to="/users" className="text-dark" onClick={handleClose}>
           <i className="bi bi-people me-2"></i>
           Users
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} to="/posts" className="text-dark">
+        <Nav.Link as={Link} to="/posts" className="text-dark" onClick={handleClose}>
           <i className="bi bi-images me-2"></i>
           Posts
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} to="/login" className="text-dark">
+        <Nav.Link as={Link} to="/login" className="text-dark" onClick={handleClose}>
           <i className="bi bi-box-arrow-in-right me-2"></i>
           Login
         </Nav.Link>
       </Nav.Item>
     </Nav>
+  );
+
+  return (
+    <>
+      {isDesktop ? (
+        <div className="bg-light sidebar vh-100 d-flex flex-column">
+          <header className="px-3 py-2 mt-3 bg-light text-black">
+            <h4 className="m-0">SPARTIGRAM</h4>
+          </header>
+          <NavContent />
+        </div>
+      ) : (
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>SPARTIGRAM</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <NavContent />
+          </Offcanvas.Body>
+        </Offcanvas>
+      )}
+    </>
   );
 };
 
